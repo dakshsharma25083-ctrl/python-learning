f047ac6ea49fd3d646ddda80a7a40a75777f4570
mrdakshsharma <dakshsharma25083@gmail.com>
2026-06-05 11:21:48 +0530

    text change

--- sch.py
+++ sch.py
@@ -5,6 +5,7 @@ A command-line Python application to manage student records using SQLite.
 
 import sqlite3
 #this is testing 
+#git test
 
 # ─────────────────────────────────────────────
 # (a) Database & Table Setup
